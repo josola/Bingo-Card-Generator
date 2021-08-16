ce623017af066048571da2888f13eb25d9272bd2
josola <jordan.sola@gmail.com>
2021-08-16 19:31:20 -0400

    Update Structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-/* src/main.cpp
- * Generates prepress ready bingo card files.
- * (c) Jordan Sola 2020 - 2021
- * Written by Jordan Sola 2020 - 2021 */
+/*
+ * Bingo card CSV file creator.
+ *
+ * (c) 2021 Jordan Sola. All rights reserved. (MIT License)
+ */
 
 #include <iostream>
 #include <random>
@@ -9,38 +10,84 @@
 #include <numeric>
 #include <fstream>
 
+
 int main()
 {
-	std::ofstream outFile;
-	
-	// Change this to whatever filename/directory you need.
-	outFile.open("./output/sample.csv");
 
-	std::vector<int> a(15), b(15), c(15), d(15), e(15);
-	int k = 0, card;
+	// Explanatory prompt
 
 	std::cout << "This program prints randomized numbers ready to merge into bingo cards.";
 
-	for (int i = 0; i < 2; i++)
-	{
+
+	// Line seperator
+
+	for (int i = 0; i < 2; i++) {
 		std::cout << std::endl;
 	}
 
+
+
+	// --- Get card count from user ---
+
+
+
+	// User input prompt
+
 	std::cout << "Please enter number of cards needed (eg. '42'): ";
 
-	while (!(std::cin >> card))
-	{
+
+	// Get count from user, bad input correction
+
+	int card_count;
+
+	while (!(std::cin >> card_count)) {
+
+
+		// Clear input stream
+
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+
+		// Prompt for input again
+
 		std::cout << "Bad value! Please type an integer (eg. '42'): ";
 	}
 
-	outFile << "A" << ',' << "B" << ',' << "C" << ',' << "D" << ',' << "E" << std::endl;
 
-	while (k < card)
-	{
+
+	// --- Card creation ---
+
+
+
+	// Create an ouput file
+
+	std::ofstream out_file;
+	out_file.open("./output/test.csv");
+
+
+	// Set csv file header
+
+	out_file << "A" << ',' << "B" << ',' << "C" << ',' << "D" << ',' << "E" << std::endl;
+
+
+	// Card columns container
+
+	std::vector<int> a(15), b(15), c(15), d(15), e(15);
+
+
+	// Create required number of cards
+
+	for (int i = 0; i < card_count; i++) {
+
+
+		// Set random seed for iota()
+
 		std::random_device rd;
 		std::mt19937 gen(rd());
+
+
+		// Create columns, shuffle columns
 
 		iota(a.begin(), a.end(), 1);
 		shuffle(a.begin(), a.end(), gen);
@@ -57,21 +104,27 @@ int main()
 		iota(e.begin(), e.end(), 61);
 		shuffle(e.begin(), e.end(), gen);
 
-		for (int i = 0; i < 5; i++)
-		{
+
+		// Fill output file with prepared content
+
+		for (int i = 0; i < 5; i++) {
+
+
+			// "FREE SPACE" goes in the center of each card
+
 			if (i == 2)
-			{
-				outFile << a[i] << ", " << b[i] << ", FREE SPACE, " << d[i] << ", " << e[i] << std::endl;
-			}
+				out_file << a[i] << ", " << b[i] << ", FREE SPACE, " << d[i] << ", " << e[i] << std::endl;
+				
 			else
-			{
-				outFile << a[i] << ", " << b[i] << ", " << c[i] << ", " << d[i] << ", " << e[i] << std::endl;
-			}
+				out_file << a[i] << ", " << b[i] << ", " << c[i] << ", " << d[i] << ", " << e[i] << std::endl;
 		}
-		k++;
 	}
 
-	outFile.close();
+
+	// Close output file for safety
+
+	out_file.close();
+
 
 	return 0;
 }
