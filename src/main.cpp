a5fb42050d71ff30ee7aa369495b2a29c488d68c
josola <jordan.sola@gmail.com>
2021-10-04 10:56:07 -0500

    Style Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Bingo card CSV file creator.
- *
  * (c) 2021 Jordan Sola. All rights reserved. (MIT License)
+ * Written by Jordan Sola 2021
  */
 
 #include <iostream>
@@ -10,85 +10,56 @@
 #include <numeric>
 #include <fstream>
 
-
-int main()
-{
-
-	// Explanatory prompt
+int main() {
 
 	std::cout << "This program prints randomized numbers ready to merge into bingo cards.";
 
-
 	// Line seperator
-
 	for (int i = 0; i < 2; i++) {
 		std::cout << std::endl;
 	}
 
 
-
 	// --- Get card count from user ---
 
 
-
 	// User input prompt
-
 	std::cout << "Please enter number of cards needed (eg. '42'): ";
 
-
 	// Get count from user, bad input correction
-
 	int card_count;
-
 	while (!(std::cin >> card_count)) {
 
-
 		// Clear input stream
-
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-
 		// Prompt for input again
-
 		std::cout << "Bad value! Please type an integer (eg. '42'): ";
 	}
-
 
 
 	// --- Card creation ---
 
 
-
 	// Create an ouput file
-
 	std::ofstream out_file;
 	out_file.open("./output/test.csv");
 
-
 	// Set csv file header
-
 	out_file << "A" << ',' << "B" << ',' << "C" << ',' << "D" << ',' << "E" << std::endl;
 
-
 	// Card columns container
-
 	std::vector<int> a(15), b(15), c(15), d(15), e(15);
 
-
 	// Create required number of cards
-
 	for (int i = 0; i < card_count; i++) {
 
-
 		// Set random seed for iota()
-
 		std::random_device rd;
 		std::mt19937 gen(rd());
 
-
 		// Create columns, shuffle columns
-
 		iota(a.begin(), a.end(), 1);
 		shuffle(a.begin(), a.end(), gen);
 
@@ -104,14 +75,10 @@ int main()
 		iota(e.begin(), e.end(), 61);
 		shuffle(e.begin(), e.end(), gen);
 
-
 		// Fill output file with prepared content
-
 		for (int i = 0; i < 5; i++) {
 
-
 			// "FREE SPACE" goes in the center of each card
-
 			if (i == 2)
 				out_file << a[i] << ", " << b[i] << ", FREE SPACE, " << d[i] << ", " << e[i] << std::endl;
 				
@@ -120,11 +87,8 @@ int main()
 		}
 	}
 
-
 	// Close output file for safety
-
 	out_file.close();
-
 
 	return 0;
 }
